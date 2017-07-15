1d64fe62e64bb3d1807b1101db51c98d5c2c05e5
DhrumilShah <dhrumil.ketul@gmail.com>
2017-07-15 08:21:30 -0400

    Initial Commit

---
+++ RealityTips/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealityTips
+//
+//  Created by Dhrumil Shah on 2017-07-15.
+//  Copyright © 2017 makeschool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
